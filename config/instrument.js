df7492f0827863f25500b7be887e94c7949b1859
Franciely Beckert <francielybeckert@gmail.com>
2024-07-01 19:56:28 +0200

    Add Sentry

---
+++ config/instrument.js
@@ -0,0 +1,19 @@
+const Sentry = require("@sentry/node");
+const { nodeProfilingIntegration } = require("@sentry/profiling-node");
+
+// Ensure to call this before requiring any other modules!
+Sentry.init({
+  dsn: "https://93456ccce7bba09e9450f2354e5073b1@o4507505085054976.ingest.de.sentry.io/4507527666794576",
+  integrations: [
+    // Add our Profiling integration
+    nodeProfilingIntegration(),
+  ],
+
+  // Add Tracing by setting tracesSampleRate
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+
+  // Set sampling rate for profiling
+  // This is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
